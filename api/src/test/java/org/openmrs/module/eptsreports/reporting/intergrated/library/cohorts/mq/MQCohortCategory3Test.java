fa65b3fdcaa3e333f1cc1651d7ab92eb27450ae8
nelson <nelson.homo@fgh.org.mz>
2021-07-30 13:09:25 +0200

    MQ Category 3 Integration Tests

---
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/library/cohorts/mq/MQCohortCategory3Test.java
@@ -0,0 +1,149 @@
+package org.openmrs.module.eptsreports.reporting.intergrated.library.cohorts.mq;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.Location;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.context.ContextAuthenticationException;
+import org.openmrs.module.eptsreports.reporting.intergrated.utils.DefinitionsFGHLiveTest;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.mq.MQCategory3CohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.mq.MQCohortQueries;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportConstants;
+import org.openmrs.module.reporting.cohort.EvaluatedCohort;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.common.DateUtil;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MQCohortCategory3Test extends DefinitionsFGHLiveTest {
+
+	@Autowired
+	MQCategory3CohortQueries mQCohortQueriesCategory3;
+
+	@Autowired
+	MQCohortQueries mQCohortQueries;
+
+	@Before
+	public void setup() throws Exception {
+		initialize();
+		executeDataSet("mq-cat3-patient-dataset.xml");
+		executeDataSet("mq-cat3-encounter-dataset.xml");
+		executeDataSet("mq-cat3-concepts-dataset.xml");
+	}
+
+	@Override
+	public void initialize() throws ContextAuthenticationException {
+		try {
+			setupWithStandardDataAndAuthentication();
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	protected Location getLocation() {
+		return Context.getLocationService().getLocation(1);
+	}
+
+	@Test
+	public void shouldFindPatientsNewlyEnrolledByAgeInAPeriodExcludingTrasferedInCategory3RF11Denominator()
+			throws EvaluationException {
+
+		final Date startInclusionDate = DateUtil.getDateTime(2019, 01, 01);
+		final Date endInclusionDate = DateUtil.getDateTime(2019, 04, 01);
+
+		final Date revisionDate = DateUtil.getDateTime(2021, 01, 01);
+
+		final Map<Parameter, Object> parameters = new HashMap<>();
+
+		parameters.put(new Parameter("location", "Location", Location.class), this.getLocation());
+		parameters.put(new Parameter(EptsReportConstants.START_INCULSION_DATE, "Start Date", Date.class),
+				startInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_INCLUSION_DATE, "End Date", Date.class), endInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_REVISION_DATE, "End Date", Date.class), revisionDate);
+
+		CohortDefinition cohortDefinition = mQCohortQueriesCategory3
+				.findPatientsNewlyEnrolledByAgeInAPeriodExcludingTrasferedInCategory3RF11Denominator();
+
+		final EvaluatedCohort evaluateCohortDefinition = this.evaluateCohortDefinition(cohortDefinition, parameters);
+
+		assertFalse(evaluateCohortDefinition.getMemberIds().isEmpty());
+
+		assertEquals(3, evaluateCohortDefinition.getMemberIds().size());
+
+	}
+
+	@Test
+	public void shouldFindPatientsWhoAreNewEnrolledOnArtByAgeUsingYearAdulyAndHaveFirstConsultInclusionPeriodCategory3RF12Numerator()
+			throws EvaluationException {
+
+		final Date startInclusionDate = DateUtil.getDateTime(2019, 01, 01);
+		final Date endInclusionDate = DateUtil.getDateTime(2019, 04, 01);
+
+		final Date revisionDate = DateUtil.getDateTime(2021, 01, 01);
+
+		final Map<Parameter, Object> parameters = new HashMap<>();
+
+		parameters.put(new Parameter("location", "Location", Location.class), this.getLocation());
+		parameters.put(new Parameter(EptsReportConstants.START_INCULSION_DATE, "Start Date", Date.class),
+				startInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_INCLUSION_DATE, "End Date", Date.class), endInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_REVISION_DATE, "End Date", Date.class), revisionDate);
+
+		CohortDefinition cohortDefinition = mQCohortQueriesCategory3
+				.findPatientsWhoAreNewEnrolledOnArtByAgeUsingYearAdulyAndHaveFirstConsultInclusionPeriodCategory3RF12Numerator();
+
+		final EvaluatedCohort evaluateCohortDefinition = this.evaluateCohortDefinition(cohortDefinition, parameters);
+
+		assertFalse(evaluateCohortDefinition.getMemberIds().isEmpty());
+
+		assertEquals(3, evaluateCohortDefinition.getMemberIds().size());
+
+	}
+
+	@Test
+	public void shouldFindPatientsWhoAreNewEnrolledOnArtByAgeUsingYearChildrenAndHaveFirstConsultInclusionPeriodCategory3RF14Numerator()
+			throws EvaluationException {
+
+		final Date startInclusionDate = DateUtil.getDateTime(2019, 01, 01);
+		final Date endInclusionDate = DateUtil.getDateTime(2019, 04, 01);
+
+		final Date revisionDate = DateUtil.getDateTime(2021, 01, 01);
+
+		final Map<Parameter, Object> parameters = new HashMap<>();
+
+		parameters.put(new Parameter("location", "Location", Location.class), this.getLocation());
+		parameters.put(new Parameter(EptsReportConstants.START_INCULSION_DATE, "Start Date", Date.class),
+				startInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_INCLUSION_DATE, "End Date", Date.class), endInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_REVISION_DATE, "End Date", Date.class), revisionDate);
+
+		CohortDefinition cohortDefinition = mQCohortQueriesCategory3
+				.findPatientsWhoAreNewEnrolledOnArtByAgeUsingYearChildrenAndHaveFirstConsultInclusionPeriodCategory3RF14Numerator();
+
+		final EvaluatedCohort evaluateCohortDefinition = this.evaluateCohortDefinition(cohortDefinition, parameters);
+
+		assertFalse(evaluateCohortDefinition.getMemberIds().isEmpty());
+
+		assertEquals(3, evaluateCohortDefinition.getMemberIds().size());
+
+	}
+
+	@Override
+	protected String username() {
+		return null;
+	}
+
+	@Override
+	protected String password() {
+		return null;
+	}
+
+}
