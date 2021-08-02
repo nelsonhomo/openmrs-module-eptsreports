dcfc075b273d84a55f18c6125b0872d80555bb91
nelson <nelson.homo@fgh.org.mz>
2021-08-02 14:31:53 +0200

    MQ Category 4 tests

---
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/library/cohorts/mq/MQCohortCategory4Test.java
@@ -0,0 +1,177 @@
+package org.openmrs.module.eptsreports.reporting.intergrated.library.cohorts.mq;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.Location;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.context.ContextAuthenticationException;
+import org.openmrs.module.eptsreports.reporting.intergrated.utils.DefinitionsFGHLiveTest;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.mq.MQCategory4CohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.mq.MQCohortQueries;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportConstants;
+import org.openmrs.module.reporting.cohort.EvaluatedCohort;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.common.DateUtil;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MQCohortCategory4Test extends DefinitionsFGHLiveTest {
+
+	@Autowired
+	MQCategory4CohortQueries mQCohortQueriesCategory4;
+
+	@Autowired
+	MQCohortQueries mQCohortQueries;
+
+	@Before
+	public void setup() throws Exception {
+		initialize();
+		executeDataSet("mq-cat4-patient-dataset.xml");
+		executeDataSet("mq-cat4-encounter-dataset.xml");
+		executeDataSet("mq-cat4-concepts-dataset.xml");
+	}
+
+	@Override
+	public void initialize() throws ContextAuthenticationException {
+		try {
+			setupWithStandardDataAndAuthentication();
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	protected Location getLocation() {
+		return Context.getLocationService().getLocation(1);
+	}
+
+	@Test
+	public void shouldFindPatientsNewlyEnrolledInAPeriodCategory4RF15Denominator() throws EvaluationException {
+
+		final Date startInclusionDate = DateUtil.getDateTime(2019, 01, 01);
+		final Date endInclusionDate = DateUtil.getDateTime(2019, 04, 01);
+
+		final Date revisionDate = DateUtil.getDateTime(2021, 01, 01);
+
+		final Map<Parameter, Object> parameters = new HashMap<>();
+
+		parameters.put(new Parameter("location", "Location", Location.class), this.getLocation());
+		parameters.put(new Parameter(EptsReportConstants.START_INCULSION_DATE, "Start Date", Date.class),
+				startInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_INCLUSION_DATE, "End Date", Date.class), endInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_REVISION_DATE, "End Date", Date.class), revisionDate);
+
+		CohortDefinition cohortDefinition = mQCohortQueriesCategory4
+				.findPatientsNewlyEnrolledInAPeriodCategory4RF15Denominator();
+
+		final EvaluatedCohort evaluateCohortDefinition = this.evaluateCohortDefinition(cohortDefinition, parameters);
+
+		assertFalse(evaluateCohortDefinition.getMemberIds().isEmpty());
+
+		assertEquals(3, evaluateCohortDefinition.getMemberIds().size());
+
+	}
+
+	@Test
+	public void shouldFindPatientsNewlyEnrolledByAgeAndNutritionalAssessmentInAPeriodCategory4RF16Numerator()
+			throws EvaluationException {
+
+		final Date startInclusionDate = DateUtil.getDateTime(2019, 01, 01);
+		final Date endInclusionDate = DateUtil.getDateTime(2019, 04, 01);
+
+		final Date revisionDate = DateUtil.getDateTime(2021, 01, 01);
+
+		final Map<Parameter, Object> parameters = new HashMap<>();
+
+		parameters.put(new Parameter("location", "Location", Location.class), this.getLocation());
+		parameters.put(new Parameter(EptsReportConstants.START_INCULSION_DATE, "Start Date", Date.class),
+				startInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_INCLUSION_DATE, "End Date", Date.class), endInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_REVISION_DATE, "End Date", Date.class), revisionDate);
+
+		CohortDefinition cohortDefinition = mQCohortQueriesCategory4
+				.findPatientsNewlyEnrolledByAgeAndNutritionalAssessmentInAPeriodCategory4RF16Numerator();
+
+		final EvaluatedCohort evaluateCohortDefinition = this.evaluateCohortDefinition(cohortDefinition, parameters);
+
+		assertFalse(evaluateCohortDefinition.getMemberIds().isEmpty());
+
+		assertEquals(3, evaluateCohortDefinition.getMemberIds().size());
+
+	}
+
+	@Test
+	public void shouldFindPregnantHumansNewlyEnrolledOnARTInInclusionPeriodCategory4RF17Denominator()
+			throws EvaluationException {
+
+		final Date startInclusionDate = DateUtil.getDateTime(2019, 01, 01);
+		final Date endInclusionDate = DateUtil.getDateTime(2019, 04, 01);
+
+		final Date revisionDate = DateUtil.getDateTime(2021, 01, 01);
+
+		final Map<Parameter, Object> parameters = new HashMap<>();
+
+		parameters.put(new Parameter("location", "Location", Location.class), this.getLocation());
+		parameters.put(new Parameter(EptsReportConstants.START_INCULSION_DATE, "Start Date", Date.class),
+				startInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_INCLUSION_DATE, "End Date", Date.class), endInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_REVISION_DATE, "End Date", Date.class), revisionDate);
+
+		CohortDefinition cohortDefinition = mQCohortQueriesCategory4
+				.findPregnantHumansNewlyEnrolledOnARTInInclusionPeriodCategory4RF17Denominator();
+
+		final EvaluatedCohort evaluateCohortDefinition = this.evaluateCohortDefinition(cohortDefinition, parameters);
+
+		assertTrue(evaluateCohortDefinition.getMemberIds().isEmpty());
+
+		assertEquals(0, evaluateCohortDefinition.getMemberIds().size());
+
+	}
+
+	@Test
+	public void shouldFindPregnantsNewlyEnrolledOnARTInInclusionPeriodAndHasNutritionalAssessmentCategory4RF18()
+			throws EvaluationException {
+
+		final Date startInclusionDate = DateUtil.getDateTime(2019, 01, 01);
+		final Date endInclusionDate = DateUtil.getDateTime(2019, 04, 01);
+
+		final Date revisionDate = DateUtil.getDateTime(2021, 01, 01);
+
+		final Map<Parameter, Object> parameters = new HashMap<>();
+
+		parameters.put(new Parameter("location", "Location", Location.class), this.getLocation());
+		parameters.put(new Parameter(EptsReportConstants.START_INCULSION_DATE, "Start Date", Date.class),
+				startInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_INCLUSION_DATE, "End Date", Date.class), endInclusionDate);
+		parameters.put(new Parameter(EptsReportConstants.END_REVISION_DATE, "End Date", Date.class), revisionDate);
+
+		CohortDefinition cohortDefinition = mQCohortQueriesCategory4
+				.findPregnantsNewlyEnrolledOnARTInInclusionPeriodAndHasNutritionalAssessmentCategory4RF18();
+
+		final EvaluatedCohort evaluateCohortDefinition = this.evaluateCohortDefinition(cohortDefinition, parameters);
+
+		assertTrue(evaluateCohortDefinition.getMemberIds().isEmpty());
+
+		assertEquals(0, evaluateCohortDefinition.getMemberIds().size());
+
+	}
+
+	@Override
+	protected String username() {
+		return null;
+	}
+
+	@Override
+	protected String password() {
+		return null;
+	}
+
+}
