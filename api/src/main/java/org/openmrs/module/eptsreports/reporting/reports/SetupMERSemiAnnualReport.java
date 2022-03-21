ed07b4d828a645f57e0e08ded420b344ab4c0446
guimino <minoneves@gmail.com>
2022-03-21 15:50:37 +0200

    fixed DQA template and MER semi-annual report description

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMERSemiAnnualReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMERSemiAnnualReport.java
@@ -24,89 +24,88 @@ import org.springframework.stereotype.Component;
 @Component
 public class SetupMERSemiAnnualReport extends EptsDataExportManager {
 
-  @Autowired private TxMlDataset txMlDataset;
+	@Autowired
+	private TxMlDataset txMlDataset;
 
-  @Autowired private GenericCohortQueries genericCohortQueries;
+	@Autowired
+	private GenericCohortQueries genericCohortQueries;
 
-  @Autowired private TxTBDataset txTBDataset;
+	@Autowired
+	private TxTBDataset txTBDataset;
 
-  @Autowired private TbPrevDataset tbPrevDataset;
+	@Autowired
+	private TbPrevDataset tbPrevDataset;
 
-  @Autowired private CxCaSCRNDataSet cxCaSCRNDataSet;
+	@Autowired
+	private CxCaSCRNDataSet cxCaSCRNDataSet;
 
-  @Autowired private CxCaTXDataSet cxCaTXDataSet;
+	@Autowired
+	private CxCaTXDataSet cxCaTXDataSet;
 
-  @Autowired private DatimCodeDataSet datimCodeDataSet;
+	@Autowired
+	private DatimCodeDataSet datimCodeDataSet;
 
-  @Override
-  public String getExcelDesignUuid() {
-    return "61fea06a-472b-11e9-8b42-876961a472ef";
-  }
+	@Override
+	public String getExcelDesignUuid() {
+		return "61fea06a-472b-11e9-8b42-876961a472ef";
+	}
 
-  @Override
-  public String getUuid() {
-    return "6febad76-472b-11e9-a41e-db8c77c788cd";
-  }
+	@Override
+	public String getUuid() {
+		return "6febad76-472b-11e9-a41e-db8c77c788cd";
+	}
 
-  @Override
-  public String getVersion() {
-    return "1.0-SNAPSHOT";
-  }
+	@Override
+	public String getVersion() {
+		return "1.0-SNAPSHOT";
+	}
 
-  @Override
-  public String getName() {
-    return "PEPFAR MER 2.6 Semi-Annual";
-  }
+	@Override
+	public String getName() {
+		return "PEPFAR MER 2.6 Semi-Annual";
+	}
 
-  @Override
-  public String getDescription() {
-    return "The TB_PREV indicator reports the proportion of ART patients who completed a standard course of TB preventive treatment within the semiannual reporting period";
-  }
+	@Override
+	public String getDescription() {
+		return "PEPFAR MER 2.6 Semi-Annual";
+	}
 
-  @Override
-  public ReportDefinition constructReportDefinition() {
-    ReportDefinition rd = new ReportDefinition();
-    rd.setUuid(getUuid());
-    rd.setName(getName());
-    rd.setDescription(getDescription());
-    rd.setParameters(txMlDataset.getParameters());
+	@Override
+	public ReportDefinition constructReportDefinition() {
+		ReportDefinition rd = new ReportDefinition();
+		rd.setUuid(getUuid());
+		rd.setName(getName());
+		rd.setDescription(getDescription());
+		rd.setParameters(txMlDataset.getParameters());
 
-    rd.addDataSetDefinition("T", Mapped.mapStraightThrough(txTBDataset.constructTxTBDataset()));
+		rd.addDataSetDefinition("T", Mapped.mapStraightThrough(txTBDataset.constructTxTBDataset()));
 
-    rd.addDataSetDefinition("TBPREV", Mapped.mapStraightThrough(tbPrevDataset.constructDatset()));
+		rd.addDataSetDefinition("TBPREV", Mapped.mapStraightThrough(tbPrevDataset.constructDatset()));
 
-    rd.addDataSetDefinition("CX", Mapped.mapStraightThrough(cxCaSCRNDataSet.constructDatset()));
-    rd.addDataSetDefinition("CXT", Mapped.mapStraightThrough(cxCaTXDataSet.constructDatset()));
-    rd.addDataSetDefinition(
-        "D",
-        Mapped.mapStraightThrough(this.datimCodeDataSet.constructDataset(this.getParameters())));
+		rd.addDataSetDefinition("CX", Mapped.mapStraightThrough(cxCaSCRNDataSet.constructDatset()));
+		rd.addDataSetDefinition("CXT", Mapped.mapStraightThrough(cxCaTXDataSet.constructDatset()));
+		rd.addDataSetDefinition("D",
+				Mapped.mapStraightThrough(this.datimCodeDataSet.constructDataset(this.getParameters())));
 
-    rd.setBaseCohortDefinition(
-        EptsReportUtils.map(
-            this.genericCohortQueries.generalSql(
-                "baseCohortQuery", BaseQueries.getBaseCohortQuery()),
-            "endDate=${endDate},location=${location}"));
-    return rd;
-  }
+		rd.setBaseCohortDefinition(EptsReportUtils.map(
+				this.genericCohortQueries.generalSql("baseCohortQuery", BaseQueries.getBaseCohortQuery()),
+				"endDate=${endDate},location=${location}"));
+		return rd;
+	}
 
-  @Override
-  public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-    ReportDesign reportDesign = null;
-    try {
-      reportDesign =
-          createXlsReportDesign(
-              reportDefinition,
-              "PEPFAR_MER_2.6_Semiannual.xls",
-              "PEPFAR MER 2.6 Semi-Annual Report",
-              getExcelDesignUuid(),
-              null);
-      Properties props = new Properties();
-      props.put("sortWeight", "5000");
-      reportDesign.setProperties(props);
-    } catch (IOException e) {
-      throw new ReportingException(e.toString());
-    }
+	@Override
+	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+		ReportDesign reportDesign = null;
+		try {
+			reportDesign = createXlsReportDesign(reportDefinition, "PEPFAR_MER_2.6_Semiannual.xls",
+					"PEPFAR MER 2.6 Semi-Annual Report", getExcelDesignUuid(), null);
+			Properties props = new Properties();
+			props.put("sortWeight", "5000");
+			reportDesign.setProperties(props);
+		} catch (IOException e) {
+			throw new ReportingException(e.toString());
+		}
 
-    return Arrays.asList(reportDesign);
-  }
+		return Arrays.asList(reportDesign);
+	}
 }
