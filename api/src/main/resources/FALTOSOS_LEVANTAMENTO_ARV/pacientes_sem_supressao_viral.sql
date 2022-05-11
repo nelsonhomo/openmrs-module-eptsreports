a80451c7eaf83c2edff429c58da3611fbffad196
nelson <nelson.homo@fgh.org.mz>
2022-05-11 16:48:09 +0200

    FALTOSOS_ARV_V2: Fix CV Disagregation Denominator

--- api/src/main/resources/FALTOSOS_LEVANTAMENTO_ARV/pacientes_sem_supressao_viral.sql
+++ api/src/main/resources/FALTOSOS_LEVANTAMENTO_ARV/pacientes_sem_supressao_viral.sql
@@ -16,7 +16,6 @@ from	(
 														from (			
 														select f.patient_id, max(f.encounter_datetime) encounter_datetime,f.ordem from (
 														 select f.patient_id,max(f.encounter_datetime) encounter_datetime,f.ordem from (																
-	
 														 select p.patient_id, max(e.encounter_datetime)  encounter_datetime, 1 as ordem from patient p
 																	inner join encounter e on e.patient_id = p.patient_id
 																	inner join obs o on o.encounter_id = e.encounter_id
@@ -38,7 +37,6 @@ from	(
 																     and e.location_id =:location and e.encounter_datetime between date_sub(:endDate, interval 12 month) and :endDate
 															     group by p.patient_id
 															     union
-															   
 															     select p.patient_id, max(o.obs_datetime)  encounter_datetime, 4 as ordem from patient p
 																	inner join encounter e on e.patient_id = p.patient_id
 																	inner join obs o on o.encounter_id = e.encounter_id
@@ -53,36 +51,41 @@ from	(
 									) cargaQuantitativa
 								left join 
 									(
-								 		select patient_id, encounter_datetime, cargaQualitativa.ordem
+									select cargaQualitativa.patient_id, max(cargaQualitativa.encounter_datetime) encounter_datetime,cargaQualitativa.ordem from (
+								 		select patient_id, max(encounter_datetime) encounter_datetime, cargaQualitativa.ordem
 										from (			
-												select p.patient_id, e.encounter_datetime, 1 as ordem from patient p
+												select p.patient_id, max(e.encounter_datetime) encounter_datetime, 1 as ordem from patient p
 													inner join encounter e on e.patient_id = p.patient_id
 													inner join obs o on o.encounter_id = e.encounter_id
 												where p.voided = 0  and e.voided = 0 and o.voided = 0 and e.encounter_type  = 13 and o.concept_id = 1305 
 														and e.location_id =:location and e.encounter_datetime between date_sub(:endDate, interval 12 month) and :endDate
+														group by p.patient_id
 												union
-												select p.patient_id, e.encounter_datetime, 2 as ordem from patient p
+												select p.patient_id, max(e.encounter_datetime) encounter_datetime, 2 as ordem from patient p
 												     inner join encounter e on e.patient_id = p.patient_id
 												     inner join obs o on o.encounter_id = e.encounter_id
 												 where p.voided = 0  and e.voided = 0 and o.voided = 0 and e.encounter_type  = 51 and o.concept_id =1305 
 												     and e.location_id =:location and e.encounter_datetime between date_sub(:endDate, interval 12 month) and :endDate
+												     group by p.patient_id
 												union
-												select p.patient_id, e.encounter_datetime  encounter_datetime, 3 as ordem from patient p
+												select p.patient_id, max(e.encounter_datetime)  encounter_datetime, 3 as ordem from patient p
 												     inner join encounter e on e.patient_id = p.patient_id
 												     inner join obs o on o.encounter_id = e.encounter_id
 												 where p.voided = 0  and e.voided = 0 and o.voided = 0 and e.encounter_type  = 6 and o.concept_id = 1305 
 												     and e.location_id =:location and e.encounter_datetime between date_sub(:endDate, interval 12 month) and :endDate
+												     group by p.patient_id
 												union
-											     select p.patient_id, o.obs_datetime obs_datetime, 4 as ordem from patient p
+											     select p.patient_id, max(o.obs_datetime) obs_datetime, 4 as ordem from patient p
 									               	inner join encounter e on e.patient_id = p.patient_id
 									              		inner join obs o on o.encounter_id = e.encounter_id
 									           	where p.voided = 0  and e.voided = 0 and o.voided = 0 and e.encounter_type  = 53 and o.concept_id = 1305 
 									               	and e.location_id =:location and o.obs_datetime between date_sub(:endDate, interval 12 month) and :endDate
-											) cargaQualitativa
+									               	group by p.patient_id 
+											) cargaQualitativa group by cargaQualitativa.patient_id, cargaQualitativa.encounter_datetime desc, ordem asc
+											)cargaQualitativa group by cargaQualitativa.patient_id 
 								 	)  cargaQualitativa on cargaQuantitativa.patient_id = cargaQualitativa.patient_id 
 			       				     where cargaQualitativa.patient_id is null  
-			       					or ( cargaQuantitativa.encounter_datetime > cargaQualitativa.encounter_datetime) 
-			       					or ( cargaQuantitativa.encounter_datetime = cargaQualitativa.encounter_datetime and cargaQuantitativa.ordem <= cargaQualitativa.ordem )  
+			       					or ( cargaQuantitativa.encounter_datetime >= cargaQualitativa.encounter_datetime )
 							) cargaQuantitativa
 							join 
 					        (
@@ -96,11 +99,8 @@ from	(
 								                                group by p.patient_id
 																   ) f 
 								inner join obs obCv on obCv.person_id=f.patient_id and obCv.concept_id=856 and obCv.value_numeric>=1000 and date(obCv.obs_datetime)=date(f.encounter_datetime)  and obCv.voided=0					
-					
 						     ) filter on filter.patient_id=cargaQuantitativa.patient_id
-						
 					    union
-						
 						select cargaQuantitativa.patient_id, cargaQuantitativa.obs_datetime encounter_datetime , 2 as ordem
 						from (
 								select cargaQuantitativa.patient_id, cargaQuantitativa.obs_datetime
