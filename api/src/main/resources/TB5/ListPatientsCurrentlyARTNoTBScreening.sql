ff4b7838daefcc6ee79faac61529baed7b42ba6f
emaposse <eusebiomaposse@gmail.com>
2022-05-23 16:28:14 +0200

    bug fix TB5 remote fixed param

--- api/src/main/resources/TB5/ListPatientsCurrentlyARTNoTBScreening.sql
+++ api/src/main/resources/TB5/ListPatientsCurrentlyARTNoTBScreening.sql
@@ -808,7 +808,7 @@ select             coorte12meses_final.patient_id,
 
             where 
 
-            (EX1.data_transferidopara is null and EX2.data_tratamento is null)  and TX_TB.data_inicio <= date_sub("2021-06-20", INTERVAL 6 MONTH)
+            (EX1.data_transferidopara is null and EX2.data_tratamento is null)  and TX_TB.data_inicio <= date_sub(:endDate, INTERVAL 6 MONTH)
 
             group by TX_TB.patient_id
 
