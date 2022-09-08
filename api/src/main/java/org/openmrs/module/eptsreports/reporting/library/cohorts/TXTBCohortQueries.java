d969cfd13131e7a43da85802883c2bf8e1aa9f6d
guimino <minoneves@gmail.com>
2022-09-08 16:27:46 +0200

    removida exclusao na exclusao do numerador do periodo anterir

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TXTBCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TXTBCohortQueries.java
@@ -441,47 +441,25 @@ public class TXTBCohortQueries {
     cd.addSearch("A", EptsReportUtils.map(A, this.generalParameterMapping));
 
     cd.addSearch(
-        "B",
+        "A-PREVIOUS-PERIOD",
         EptsReportUtils.map(
             A, "startDate=${startDate-6m},endDate=${startDate-1d},location=${location}"));
 
-    cd.setCompositionString("A NOT B");
+    cd.setCompositionString("A NOT A-PREVIOUS-PERIOD");
 
     this.addGeneralParameters(cd);
     return cd;
   }
 
-  public CohortDefinition getNumeratorPreviosPeriod() {
+  @DocumentedDefinition(value = "txTbNumerator")
+  public CohortDefinition getNumeratorForPreviousPeriod() {
     final CompositionCohortDefinition cd = new CompositionCohortDefinition();
-    cd.setName("TxTB - txTbNumerator Previous Period");
-    final CohortDefinition i =
-        this.genericCohortQueries.generalSql(
-            "onTbTreatment-Previous Reporting Period",
-            TXTBQueries.dateObsForPreviousReportingPeriod(
-                this.tbMetadata.getTBDrugTreatmentStartDate().getConceptId(),
-                Arrays.asList(
-                    this.hivMetadata.getAdultoSeguimentoEncounterType().getId(),
-                    this.hivMetadata.getARVPediatriaSeguimentoEncounterType().getId()),
-                true));
-    final CohortDefinition ii = this.getInTBProgramPreviousPeriod();
-    this.addGeneralParameters(i);
-    cd.addSearch("i", this.map(i, this.generalParameterMapping));
-    cd.addSearch("ii", this.map(ii, this.generalParameterMapping));
-    cd.addSearch(
-        "iii", this.map(this.getPulmonaryTBWithinReportingDate(), this.generalParameterMapping));
-    cd.addSearch(
-        "iv",
-        this.map(
-            this.getTuberculosisTreatmentPlanWithinPreviousReportingDate(),
-            this.generalParameterMapping));
+    cd.setName("TxTB - Numerator for Previous Period");
+    final CohortDefinition A = this.txTbNumeratorA();
+    cd.addSearch("A", EptsReportUtils.map(A, this.generalParameterMapping));
 
-    cd.addSearch(
-        "started-tb-treatment-previous-period",
-        EptsReportUtils.map(
-            this.getTbDrugTreatmentStartDateWithinReportingDate(),
-            "startDate=${startDate-12m},endDate=${startDate-6m-1d},location=${location}"));
+    cd.setCompositionString("A");
 
-    cd.setCompositionString("(i OR ii OR iii OR iv) NOT started-tb-treatment-previous-period");
     this.addGeneralParameters(cd);
     return cd;
   }
@@ -691,7 +669,7 @@ public class TXTBCohortQueries {
     definition.addSearch(
         "A-PREVIOUS-PERIOD",
         EptsReportUtils.map(
-            this.txTbNumerator(),
+            this.getNumeratorForPreviousPeriod(),
             "startDate=${startDate-6m},endDate=${startDate-1d},location=${location}"));
 
     definition.addSearch(
