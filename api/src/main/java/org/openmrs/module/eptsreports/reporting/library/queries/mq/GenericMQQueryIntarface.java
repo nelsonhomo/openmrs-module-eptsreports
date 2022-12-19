53327aa07fa872b76aa09a8b69833fc387285c50
emaposse <eusebiomaposse@gmail.com>
2022-12-19 11:34:38 +0200

    Change way who find art initiation date.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/mq/GenericMQQueryIntarface.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/mq/GenericMQQueryIntarface.java
@@ -50,20 +50,25 @@ public interface GenericMQQueryIntarface {
     public static final String findPatientsWhoAreNewlyEnrolledOnARTBiggerThanParam(int startAge) {
 
       final String sql =
-          " SELECT patient_id FROM ( "
-              + " SELECT patient_id, MIN(art_start_date) art_start_date FROM ( "
-              + " SELECT p.patient_id, MIN(value_datetime) art_start_date FROM patient p "
-              + " INNER JOIN encounter e ON p.patient_id = e.patient_id "
-              + " INNER JOIN obs o ON e.encounter_id = o.encounter_id "
-              + " WHERE p.voided = 0 AND e.voided = 0 AND o.voided = 0 AND e.encounter_type = 53 "
-              + " AND o.concept_id = 1190 AND o.value_datetime is NOT NULL AND o.value_datetime <= :endInclusionDate AND e.location_id = :location "
-              + " GROUP BY p.patient_id "
-              + " ) art_start "
-              + " GROUP BY patient_id "
-              + " ) tx_new "
-              + "INNER JOIN person pe ON tx_new.patient_id=pe.person_id "
-              + "WHERE (TIMESTAMPDIFF(year,birthdate,art_start_date)) >= %s AND birthdate IS NOT NULL and pe.voided = 0  "
-              + "AND art_start_date BETWEEN :startInclusionDate AND :endInclusionDate ";
+          "select f.patient_id from   ( "
+              + "SELECT tx_new.patient_id,tx_new.art_start_date,(TIMESTAMPDIFF(year,pe.birthdate,art_start_date)) FROM  "
+              + "( "
+              + "SELECT patient_id, MIN(art_start_date) art_start_date FROM (  "
+              + "SELECT p.patient_id, MIN(value_datetime) art_start_date FROM patient p  "
+              + "INNER JOIN encounter e ON p.patient_id = e.patient_id  "
+              + "INNER JOIN obs o ON e.encounter_id = o.encounter_id  "
+              + "WHERE p.voided = 0 AND e.voided = 0 AND o.voided = 0 AND e.encounter_type = 53  "
+              + "AND o.concept_id = 1190  "
+              + "AND o.value_datetime is NOT NULL AND o.value_datetime <= :endInclusionDate   "
+              + "AND e.location_id = :location "
+              + "GROUP BY p.patient_id  "
+              + ") art_start  "
+              + "GROUP BY patient_id  "
+              + ") tx_new  "
+              + "INNER JOIN person pe ON tx_new.patient_id=pe.person_id  "
+              + "WHERE (TIMESTAMPDIFF(year,pe.birthdate,art_start_date)) >= %s AND pe.birthdate IS NOT NULL and pe.voided = 0   "
+              + "AND art_start_date BETWEEN  :startInclusionDate AND :endInclusionDate "
+              + ")f ";
 
       return String.format(sql, startAge);
     }
