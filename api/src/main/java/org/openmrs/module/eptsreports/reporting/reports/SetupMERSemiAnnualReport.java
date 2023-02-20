209a1a4687f660e202da9258bd507f254d993287
emaposse <eusebiomaposse@gmail.com>
2023-02-20 15:09:34 +0200

    SESP-10:Bug Fix.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMERSemiAnnualReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMERSemiAnnualReport.java
@@ -55,12 +55,12 @@ public class SetupMERSemiAnnualReport extends EptsDataExportManager {
 
   @Override
   public String getName() {
-    return "PEPFAR MER 2.6 Semi-Annual";
+    return "PEPFAR MER 2.6.1 Semi-Annual";
   }
 
   @Override
   public String getDescription() {
-    return "PEPFAR MER 2.6 Semi-Annual";
+    return "PEPFAR MER 2.6.1 Semi-Annual";
   }
 
   @Override
@@ -71,9 +71,9 @@ public class SetupMERSemiAnnualReport extends EptsDataExportManager {
     rd.setDescription(getDescription());
     rd.setParameters(txMlDataset.getParameters());
     rd.addDataSetDefinition("T", Mapped.mapStraightThrough(txTBDataset.constructTxTBDataset()));
-	rd.addDataSetDefinition("TBPREV", Mapped.mapStraightThrough(tbPrevDataset.constructDatset()));
-	rd.addDataSetDefinition("CX", Mapped.mapStraightThrough(cxCaSCRNDataSet.constructDatset()));
-	rd.addDataSetDefinition("CXT", Mapped.mapStraightThrough(cxCaTXDataSet.constructDatset()));
+    rd.addDataSetDefinition("TBPREV", Mapped.mapStraightThrough(tbPrevDataset.constructDatset()));
+    rd.addDataSetDefinition("CX", Mapped.mapStraightThrough(cxCaSCRNDataSet.constructDatset()));
+    rd.addDataSetDefinition("CXT", Mapped.mapStraightThrough(cxCaTXDataSet.constructDatset()));
     rd.addDataSetDefinition(
         "D",
         Mapped.mapStraightThrough(this.datimCodeDataSet.constructDataset(this.getParameters())));
@@ -93,8 +93,8 @@ public class SetupMERSemiAnnualReport extends EptsDataExportManager {
       reportDesign =
           createXlsReportDesign(
               reportDefinition,
-              "PEPFAR_MER_2.6_Semiannual.xls",
-              "PEPFAR MER 2.6 Semi-Annual Report",
+              "PEPFAR_MER_2.6.1_Quarterly.xls",
+              "PEPFAR MER 2.6.1 Semi-Annual Report",
               getExcelDesignUuid(),
               null);
       Properties props = new Properties();
