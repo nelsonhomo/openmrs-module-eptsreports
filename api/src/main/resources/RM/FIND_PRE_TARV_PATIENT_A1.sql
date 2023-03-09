e1451017532a2b7cb7df41ca7bed461eff377278
emaposse <eusebiomaposse@gmail.com>
2023-03-09 16:17:44 +0200

    SESP-33: Bug Fix A1 and A2.

--- api/src/main/resources/RM/FIND_PRE_TARV_PATIENT_A1.sql
+++ api/src/main/resources/RM/FIND_PRE_TARV_PATIENT_A1.sql
@@ -4,7 +4,8 @@
             SELECT p.patient_id,MIN(o.value_datetime) AS initialDate FROM patient p   
             INNER JOIN encounter e  ON e.patient_id=p.patient_id  
             INNER JOIN obs o on o.encounter_id=e.encounter_id  
-            WHERE e.voided=0 AND o.voided=0 AND e.encounter_type=53 AND e.location_id=:location AND o.value_datetime IS NOT NULL AND o.concept_id=23808 AND o.value_datetime<:startDate  
+            WHERE e.voided=0 AND o.voided=0 AND e.encounter_type=53 AND e.location_id=:location AND o.value_datetime IS NOT NULL 
+            AND o.concept_id=23808 AND o.value_datetime<:startDate  
             GROUP BY p.patient_id  
             UNION 
             SELECT p.patient_id,min(e.encounter_datetime) AS initialDate FROM patient p   
@@ -30,12 +31,13 @@
             SELECT p.patient_id,MIN(o.value_datetime) AS initialDate FROM patient p   
             INNER JOIN encounter e  ON e.patient_id=p.patient_id  
             INNER JOIN obs o on o.encounter_id=e.encounter_id  
-            WHERE e.voided=0 AND o.voided=0 AND e.encounter_type=53 AND e.location_id=:location AND o.value_datetime IS NOT NULL AND o.concept_id=1190 AND o.value_datetime<:startDate  
+            WHERE e.voided=0 AND o.voided=0 AND e.encounter_type=53 AND e.location_id=:location AND o.value_datetime IS NOT NULL 
+            AND o.concept_id=1190 AND o.value_datetime<:startDate  
             GROUP BY p.patient_id  
             UNION 
             SELECT pg.patient_id, min(pg.date_enrolled)  AS initialDate FROM patient p  
             INNER JOIN patient_program pg on pg.patient_id=p.patient_id  
-            WHERE pg.program_id=2 AND pg.location_id=:location AND pg.voided=0 AND pg.date_enrolled<:startDate  and p.patient_id=18842
+            WHERE pg.program_id=2 AND pg.location_id=:location AND pg.voided=0 AND pg.date_enrolled<:startDate 
             GROUP BY patient_id 
             
             )tarvFinal
@@ -47,7 +49,8 @@
                SELECT p.patient_id,MIN(o.value_datetime) AS initialDate FROM patient p   
                INNER JOIN encounter e  ON e.patient_id=p.patient_id  
                INNER JOIN obs o on o.encounter_id=e.encounter_id  
-               WHERE e.voided=0 AND o.voided=0 AND e.encounter_type=53 AND e.location_id=:location AND o.value_datetime IS NOT NULL AND o.concept_id=23808 AND o.value_datetime<:startDate  
+               WHERE e.voided=0 AND o.voided=0 AND e.encounter_type=53 AND e.location_id=:location AND o.value_datetime IS NOT NULL 
+               AND o.concept_id=23808 AND o.value_datetime<:startDate  
                GROUP BY p.patient_id  
                UNION 
                SELECT p.patient_id,min(e.encounter_datetime) AS initialDate FROM patient p   
