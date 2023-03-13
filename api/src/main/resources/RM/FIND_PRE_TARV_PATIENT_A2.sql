16fb7e8b9ae438db869537d695c147e3f3799f9f
emaposse <eusebiomaposse@gmail.com>
2023-03-13 10:42:04 +0200

    SESP-33-V2:Bug Fix A2.

--- api/src/main/resources/RM/FIND_PRE_TARV_PATIENT_A2.sql
+++ api/src/main/resources/RM/FIND_PRE_TARV_PATIENT_A2.sql
@@ -5,19 +5,19 @@
             INNER JOIN encounter e  ON e.patient_id=p.patient_id  
             INNER JOIN obs o on o.encounter_id=e.encounter_id  
             WHERE e.voided=0 AND o.voided=0 AND e.encounter_type=53 AND e.location_id=:location AND o.value_datetime IS NOT NULL 
-            AND o.concept_id=23808 AND o.value_datetime<:endDate  
+            AND o.concept_id=23808 AND o.value_datetime<=:endDate  
             GROUP BY p.patient_id  
             UNION 
             SELECT p.patient_id,min(e.encounter_datetime) AS initialDate FROM patient p   
             INNER JOIN encounter e  ON e.patient_id=p.patient_id  
             INNER JOIN obs o on o.encounter_id=e.encounter_id  
             WHERE e.voided=0 AND o.voided=0 AND e.encounter_type IN(5,7)  
-            AND e.location_id=:location  AND e.encounter_datetime<:endDate  
+            AND e.location_id=:location  AND e.encounter_datetime<=:endDate  
             GROUP BY p.patient_id  
             UNION  
             SELECT pg.patient_id, min(pg.date_enrolled)  AS initialDate FROM patient p  
             INNER JOIN patient_program pg on pg.patient_id=p.patient_id  
-            WHERE pg.program_id=1 AND pg.location_id=:location AND pg.voided=0 AND pg.date_enrolled<:endDate  
+            WHERE pg.program_id=1 AND pg.location_id=:location AND pg.voided=0 AND pg.date_enrolled<=:endDate  
             GROUP BY patient_id 
             )preTarv             
 
@@ -32,12 +32,12 @@
             INNER JOIN encounter e  ON e.patient_id=p.patient_id  
             INNER JOIN obs o on o.encounter_id=e.encounter_id  
             WHERE e.voided=0 AND o.voided=0 AND e.encounter_type=53 AND e.location_id=:location AND o.value_datetime IS NOT NULL 
-            AND o.concept_id=1190 AND o.value_datetime<:endDate  
+            AND o.concept_id=1190 AND o.value_datetime<=:endDate  
             GROUP BY p.patient_id  
             UNION 
             SELECT pg.patient_id, min(pg.date_enrolled)  AS initialDate FROM patient p  
             INNER JOIN patient_program pg on pg.patient_id=p.patient_id  
-            WHERE pg.program_id=2 AND pg.location_id=:location AND pg.voided=0 AND pg.date_enrolled<:endDate 
+            WHERE pg.program_id=2 AND pg.location_id=:location AND pg.voided=0 AND pg.date_enrolled<=:endDate 
             GROUP BY patient_id 
             
             )tarvFinal
@@ -50,19 +50,19 @@
                INNER JOIN encounter e  ON e.patient_id=p.patient_id  
                INNER JOIN obs o on o.encounter_id=e.encounter_id  
                WHERE e.voided=0 AND o.voided=0 AND e.encounter_type=53 AND e.location_id=:location AND o.value_datetime IS NOT NULL 
-               AND o.concept_id=23808 AND o.value_datetime<:endDate  
+               AND o.concept_id=23808 AND o.value_datetime<=:endDate  
                GROUP BY p.patient_id  
                UNION 
                SELECT p.patient_id,min(e.encounter_datetime) AS initialDate FROM patient p   
                INNER JOIN encounter e  ON e.patient_id=p.patient_id  
                INNER JOIN obs o on o.encounter_id=e.encounter_id  
                WHERE e.voided=0 AND o.voided=0 AND e.encounter_type IN(5,7)  
-               AND e.location_id=:location  AND e.encounter_datetime<:endDate  
+               AND e.location_id=:location  AND e.encounter_datetime<=:endDate  
                GROUP BY p.patient_id  
                UNION  
                SELECT pg.patient_id, min(pg.date_enrolled)  AS initialDate FROM patient p  
                INNER JOIN patient_program pg on pg.patient_id=p.patient_id  
-               WHERE pg.program_id=1 AND pg.location_id=:location AND pg.voided=0 AND pg.date_enrolled<:endDate  
+               WHERE pg.program_id=1 AND pg.location_id=:location AND pg.voided=0 AND pg.date_enrolled<=:endDate  
                GROUP BY patient_id 
                )preTarv                
                )
