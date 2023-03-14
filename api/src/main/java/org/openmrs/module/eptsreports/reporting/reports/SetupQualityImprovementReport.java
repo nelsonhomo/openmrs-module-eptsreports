47922eb8431349ff1b45f078e60a96021f46491e
emaposse <eusebiomaposse@gmail.com>
2023-03-14 16:36:05 +0200

    SESP:Bug Fix Cat 18 and Cat 9.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupQualityImprovementReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupQualityImprovementReport.java
@@ -76,7 +76,7 @@ public class SetupQualityImprovementReport extends EptsDataExportManager {
           createXlsReportDesign(
               reportDefinition,
               "MQ.xls",
-              "Melhoria de Qualidade - 2022",
+              "Melhoria de Qualidade - 2023",
               getExcelDesignUuid(),
               null);
       Properties props = new Properties();
