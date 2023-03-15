22398ac6160bc19ee90cbc23fe0f092d4b12399e
emaposse <eusebiomaposse@gmail.com>
2023-03-15 13:43:16 +0200

    Change way to find inicio de tarv.

--- api/src/main/resources/DQA/NUMBER_OF_ACTIVE_PATIENTS_IN_ART_WHO_RECEIVED_VIRAL_LOAD_TEST_RESULT.sql
+++ api/src/main/resources/DQA/NUMBER_OF_ACTIVE_PATIENTS_IN_ART_WHO_RECEIVED_VIRAL_LOAD_TEST_RESULT.sql
@@ -1,5 +1,5 @@
     
-select (@cnt := @cnt + 1) as ID,cv.patient_id,cv.data_usar,cv.DATA_INICIO_TARV,cv.DATA_CV,cv.CV_Qualitativa,cv.CV_Quantitativa      
+select (@cnt := @cnt + 1) as ID,cv.patient_id,cv.data_usar,artInitiation.data_inicio,cv.DATA_CV,cv.CV_Qualitativa,cv.CV_Quantitativa      
 ,pid.identifier as NID,pe.gender SEXO,round(datediff(:endDate,pe.birthdate)/365) as AGE
  from (
 
@@ -737,3 +737,42 @@ select inicio_fila_seg_prox.*,
               where pid1.patient_id=pid2.patient_id and pid1.patient_identifier_id=pid2.id 
             ) pid on pid.patient_id=cv.patient_id 
             CROSS JOIN (SELECT @cnt := 0) as DEMMY
+            inner join
+            (
+            Select patient_id,min(data_inicio) data_inicio from 
+           ( 
+            Select p.patient_id,min(e.encounter_datetime) data_inicio from patient p  
+            inner join encounter e on p.patient_id=e.patient_id 
+            inner join obs o on o.encounter_id=e.encounter_id
+            inner join person pe on pe.person_id = p.patient_id
+            where e.voided=0 and o.voided=0 and p.voided=0 and pe.voided = 0 and e.encounter_type in (18,6,9) and o.concept_id=1255 and o.value_coded=1256 and  e.encounter_datetime<=:endDate  and e.location_id=:location 
+            group by p.patient_id 
+            union 
+            Select p.patient_id,min(value_datetime) data_inicio from patient p 
+            inner join encounter e on p.patient_id=e.patient_id 
+            inner join obs o on e.encounter_id=o.encounter_id
+            inner join person pe on pe.person_id = p.patient_id
+            where p.voided=0 and pe.voided=0 and e.voided=0 and o.voided=0 and e.encounter_type in (18,6,9,53) and  o.concept_id=1190 and o.value_datetime is not null and  o.value_datetime <=:endDate  and e.location_id=:location group by p.patient_id 
+            union 
+            select pg.patient_id,min(date_enrolled) data_inicio from 
+            patient p 
+            inner join patient_program pg on p.patient_id=pg.patient_id 
+            inner join person pe on pe.person_id = p.patient_id
+            where pg.voided=0 and p.voided=0 and pe.voided = 0 and program_id=2 and date_enrolled <=:endDate  and location_id=:location 
+            group by pg.patient_id 
+            union 
+            SELECT e.patient_id, MIN(e.encounter_datetime) AS data_inicio  FROM   patient p 
+            inner join encounter e on p.patient_id=e.patient_id 
+            inner join person pe on pe.person_id = p.patient_id
+            WHERE p.voided=0 and pe.voided = 0 and e.encounter_type=18 AND e.voided=0 and e.encounter_datetime <=:endDate  and e.location_id=:location 
+            GROUP BY p.patient_id 
+            union 
+            Select p.patient_id,min(value_datetime) data_inicio from patient p 
+            inner join encounter e on p.patient_id=e.patient_id 
+            inner join obs o on e.encounter_id=o.encounter_id 
+            inner join person pe on pe.person_id = p.patient_id
+            where p.voided=0 and pe.voided=0 and e.voided=0 and o.voided=0 and e.encounter_type=52 and  o.concept_id=23866 and o.value_datetime is not null and  o.value_datetime <=:endDate  and e.location_id=:location 
+            group by p.patient_id
+            ) artInitiation 
+            group by patient_id
+            )artInitiation on artInitiation.patient_id=cv.patient_id
