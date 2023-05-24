2444fdc4644d0cd149304e2d9e71864a9c6ec57f
emaposse <eusebiomaposse@gmail.com>
2023-05-24 09:20:35 +0200

    Bug Fix 9.2 CCS Data Base.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/mq/MQCategory9QueriesInterface.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/mq/MQCategory9QueriesInterface.java
@@ -144,11 +144,12 @@ public interface MQCategory9QueriesInterface {
                 + "where p.voided=0 and e.voided=0 and e.encounter_type=6 and e.location_id=:location   "
                 + "group by p.patient_id  "
                 + ") firstClinica  "
-                + "inner join obs obsCD4 on obsCD4.person_id=firstClinica.patient_id  "
+                + "inner join encounter e on firstClinica.patient_id=e.patient_id  "
+                + "inner join obs obsCD4 on obsCD4.encounter_id=e.encounter_id  "
                 + "where firstClinica.encounter_datetime between  DATE_ADD(DATE_SUB(:endRevisionDate, INTERVAL 12 MONTH), INTERVAL 1 DAY) and DATE_SUB(:endRevisionDate, INTERVAL 9 MONTH)  "
                 + "and obsCD4.obs_datetime > firstClinica.encounter_datetime and obsCD4.obs_datetime <=  DATE_ADD(firstClinica.encounter_datetime, INTERVAL 33 DAY)  "
                 + "and obsCD4.concept_id in(1695,703) and obsCD4.value_numeric is not null and obsCD4.voided=0  "
-                + "and obsCD4.location_id=:location ";
+                + "and obsCD4.location_id=:location and e.encounter_type=6 ";
 
     public static final String
         findPregnantWomanWhithCD4ResultOn33DaysAfterFirstClinicalConsultationDuringInclusionDateNumeratorCategory9 =
