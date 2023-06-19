22e65e704309fb65dc0357346fa1a7e57368470a
guimino <minoneves@gmail.com>
2023-06-19 20:14:29 +0200

    removed unused report and update metadata dump script

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupPatientEnrolledInArtProgramReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupPatientEnrolledInArtProgramReport.java
@@ -16,6 +16,7 @@ import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Deprecated
 @Component
 public class SetupPatientEnrolledInArtProgramReport extends EptsDataExportManager {
   @Autowired protected GenericCohortQueries genericCohortQueries;
