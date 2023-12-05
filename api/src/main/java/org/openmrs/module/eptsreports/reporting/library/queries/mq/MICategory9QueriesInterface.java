26a8a259749af353cadb2aee61ad50e898462ed6
emaposse <eusebiomaposse@gmail.com>
2023-12-05 16:25:29 +0200

    Bug Fix MI Category 9.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/mq/MICategory9QueriesInterface.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/mq/MICategory9QueriesInterface.java
@@ -173,7 +173,7 @@ public interface MICategory9QueriesInterface {
                 + "and obsCD4.value_numeric is not null   "
                 + "and obsCD4.voided=0   "
                 + "and obsCD4.obs_datetime >=firstClinica.encounter_datetime  "
-                + "and obsCD4.obs_datetime<date_add(firstClinica.encounter_datetime, interval 33 day)   "
+                + "and obsCD4.obs_datetime<=date_add(firstClinica.encounter_datetime, interval 33 day)   "
                 + "and obsCD4.location_id=:location "
                 + "and e.encounter_type=6 ";
 
