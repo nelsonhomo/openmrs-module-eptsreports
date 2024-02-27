6d76b00ec2646e6109300ab1b6bcf83acd6fd320
emaposse <eusebiomaposse@gmail.com>
2024-02-27 10:06:33 +0200

    Bug Fix RM.

--- api/src/main/resources/RMB12/PATIENTS_WHO_ARE_CURRENTLY_ENROLLED_ON_ART_B12.sql
+++ api/src/main/resources/RMB12/PATIENTS_WHO_ARE_CURRENTLY_ENROLLED_ON_ART_B12.sql
@@ -55,7 +55,7 @@
                              ) max_estado                                                                                                                        
                                  inner join patient_program pp on pp.patient_id = max_estado.patient_id                                                          
                                  inner join patient_state ps on ps.patient_program_id = pp.patient_program_id and ps.start_date = max_estado.data_estado         
-                             where pp.program_id = 2 and ps.state = 8 and pp.voided = 0 and ps.voided = 0 and pp.location_id = 277                 
+                             where pp.program_id = 2 and ps.state = 8 and pp.voided = 0 and ps.voided = 0 and pp.location_id =:location                 
                              
 							 union                                                                                                                               
                              
@@ -77,7 +77,7 @@
 											) max_estado                                                                                                                        
 												inner join patient_program pp on pp.patient_id = max_estado.patient_id                                                          
 												inner join patient_state ps on ps.patient_program_id = pp.patient_program_id and ps.start_date = max_estado.data_estado         
-											where pp.program_id = 2 and ps.state = 10 and pp.voided = 0 and ps.voided = 0 and pp.location_id = 277  
+											where pp.program_id = 2 and ps.state = 10 and pp.voided = 0 and ps.voided = 0 and pp.location_id =:location  
 											 union
 											select  p.patient_id,                                                                                                               
 												max(o.obs_datetime) data_estado                                                                                             
@@ -93,17 +93,7 @@
 											select person_id as patient_id,death_date as data_estado                                                                            
 											from person                                                                                                                         
 											where dead=1 and voided = 0 and death_date is not null and death_date<:startDate 
-											union                                                                                                                               
-											select  p.patient_id,                                                                                                               
-												max(obsObito.obs_datetime) data_estado                                                                                      
-											from patient p                                                                                                                   
-												inner join person pe on pe.person_id = p.patient_id                                                                         
-												inner join encounter e on p.patient_id=e.patient_id                                                                         
-												inner join obs obsObito on e.encounter_id=obsObito.encounter_id                                                             
-											where e.voided=0 and p.voided=0 and pe.voided = 0 and obsObito.voided=0                                                        
-												and e.encounter_type in (21,36,37) and  e.encounter_datetime<:startDate and  e.location_id=:location                          
-												and obsObito.concept_id in (2031,23944,23945) and obsObito.value_coded=1366                                                     
-											group by p.patient_id                                                                                                               
+					                                                                                                        
 									) dead_state group by dead_state.patient_id  
 							) dead_state 
 							inner join
@@ -162,7 +152,7 @@
 			                             		) max_estado                                                                                                                        
 			                                 		inner join patient_program pp on pp.patient_id = max_estado.patient_id                                                          
 			                                 		inner join patient_state ps on ps.patient_program_id = pp.patient_program_id and ps.start_date = max_estado.data_estado         
-			                             		where pp.program_id = 2 and ps.state = 7 and pp.voided = 0 and ps.voided = 0 and pp.location_id = 277                 
+			                             		where pp.program_id = 2 and ps.state = 7 and pp.voided = 0 and ps.voided = 0 and pp.location_id =:location                 
 			                             
 			                             		union                                                                                                                               
 			                             		
@@ -175,23 +165,6 @@
 			                                   	and e.encounter_type in (53,6) and o.concept_id in (6272,6273) and o.value_coded = 1706                         
 			                                     	and o.obs_datetime<:startDate and e.location_id=:location                                                                        
 			                             			group by p.patient_id                                                                                                               
-			                             		
-			                             	     union                                                                                                                               
-				                             
-				                             	select ultimaBusca.patient_id, ultimaBusca.data_estado                                                                              
-				                             	from (                                                                                                                              
-				                                     select p.patient_id,max(e.encounter_datetime) data_estado                                                                   
-				                                     from patient p                                                                                                              
-				                                         inner join person pe on pe.person_id = p.patient_id                                                                     
-				                                         inner join encounter e on p.patient_id=e.patient_id                                                                     
-				                                         inner join obs o on o.encounter_id=e.encounter_id                                                                       
-				                                     where e.voided=0 and p.voided=0 and pe.voided = 0 and e.encounter_datetime<:startDate                                       
-				                                         and e.encounter_type = 21 and  e.location_id= 277                                                                 
-				                                         group by p.patient_id                                                                                                   
-				                                 ) ultimaBusca                                                                                                                   
-				                                     inner join encounter e on e.patient_id = ultimaBusca.patient_id                                                             
-				                                     inner join obs o on o.encounter_id = e.encounter_id                                                                         
-				                                where e.encounter_type = 21 and o.voided=0 and o.concept_id=2016 and o.value_coded in (1706,23863) and ultimaBusca.data_estado = e.encounter_datetime and e.location_id = 277 
 		                                ) saidas_por_transferencia 
 	                                	group by patient_id 
                                	) saidas_por_transferencia
