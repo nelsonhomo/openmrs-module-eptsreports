f5d32013156c1ac5b798175fea25bebb3a7d88c9
guimino <minoneves@gmail.com>
2024-04-17 16:12:03 +0200

    SESP-220: revertida a logica de busca do ultimo CD4 (DAH_RF4)

--- api/src/main/resources/LIST_PATIENTS_WITH_DAH/PATIENTS_ELEGIBLE_TO_FOLLOWUP_DAH_LIST.sql
+++ api/src/main/resources/LIST_PATIENTS_WITH_DAH/PATIENTS_ELEGIBLE_TO_FOLLOWUP_DAH_LIST.sql
@@ -1816,74 +1816,51 @@ left join
 ( 
 	select patient_id,max(max_data_cd4) max_data_cd4, value_numeric 
 	from( 
-		select *
-		from(
-			select max_cd4.patient_id, max(max_cd4.data_cd4) max_data_cd4 , max_cd4.value_numeric
-			from(
-			
-				select list_cd4.patient_id, list_cd4.data_cd4, list_cd4.value_numeric  
-				from(
-					select p.patient_id ,o.obs_datetime data_cd4, o.value_numeric  
-					from patient p 
-						inner join encounter e on p.patient_id=e.patient_id 
-						inner join obs o on e.encounter_id=o.encounter_id 
-					where p.voided=0 and e.voided=0 and o.voided=0 and concept_id = 1695 and  e.encounter_type in (6,51,13,53,90) 
-					and o.obs_datetime between :startDate and :endDate and e.location_id=:location 
-				) list_cd4
-					inner join person per on per.person_id=list_cd4.patient_id 
-				    	inner join obs o on o.person_id=list_cd4.patient_id and list_cd4.data_cd4=o.obs_datetime and o.voided=0  
-				    	and per.voided=0 and timestampdiff(year,per.birthdate,:endDate)>=5 and o.concept_id = 1695 and o.value_numeric<200 and o.location_id=:location 
-			)
-			max_cd4 group by patient_id
-        	) max_cd4
+		select distinct max_cd4.patient_id, max_data_cd4,o.value_numeric 
+		from( 
+    			select p.patient_id,max(o.obs_datetime) max_data_cd4  
+    			from patient p 
+    				inner join encounter e on p.patient_id=e.patient_id 
+    				inner join obs o on e.encounter_id=o.encounter_id 
+    			where p.voided=0 and e.voided=0 and o.voided=0 and concept_id = 1695 and  e.encounter_type in (6,51,13,53,90) 
+    				and o.obs_datetime between :startDate and :endDate and e.location_id=:location 
+    				group by p.patient_id 
+    		)max_cd4 
+    			inner join person per on per.person_id=max_cd4.patient_id 
+    			inner join obs o on o.person_id=max_cd4.patient_id and max_cd4.max_data_cd4=o.obs_datetime and o.voided=0  
+    			and per.voided=0 and timestampdiff(year,per.birthdate,:endDate)>=5 and o.concept_id = 1695 and o.value_numeric<200 and o.location_id=:location 
         	
         	union 
-
-        	select *
-		from(
-			select max_cd4.patient_id, max(max_cd4.data_cd4) max_data_cd4 , max_cd4.value_numeric
-			from(
-			
-				select list_cd4.patient_id, list_cd4.data_cd4, list_cd4.value_numeric  
-				from(
-					select p.patient_id ,o.obs_datetime data_cd4, o.value_numeric  
-					from patient p 
-						inner join encounter e on p.patient_id=e.patient_id 
-						inner join obs o on e.encounter_id=o.encounter_id 
-					where p.voided=0 and e.voided=0 and o.voided=0 and concept_id = 1695 and  e.encounter_type in (6,51,13,53,90) 
-					and o.obs_datetime between :startDate and :endDate and e.location_id=:location 
-				) list_cd4
-					inner join person per on per.person_id=list_cd4.patient_id 
-		    			inner join obs o on o.person_id=list_cd4.patient_id and list_cd4.data_cd4=o.obs_datetime and o.voided=0  
-		   			and per.voided=0 and timestampdiff(month,per.birthdate,:endDate)>=12 and timestampdiff(year,per.birthdate,:endDate)<5 and o.concept_id = 1695 and o.value_numeric<500 and o.location_id=:location 
-            
-			)
-			max_cd4 group by patient_id
-        	) max_cd4
-
-        	union
        	
-       	select *
-		from(
-			select max_cd4.patient_id, max(max_cd4.data_cd4) max_data_cd4 , max_cd4.value_numeric
-			from(
-			
-				select list_cd4.patient_id, list_cd4.data_cd4, list_cd4.value_numeric  
-				from(
-					select p.patient_id ,o.obs_datetime data_cd4, o.value_numeric  
-					from patient p 
-						inner join encounter e on p.patient_id=e.patient_id 
-						inner join obs o on e.encounter_id=o.encounter_id 
-					where p.voided=0 and e.voided=0 and o.voided=0 and concept_id = 1695 and  e.encounter_type in (6,51,13,53,90) 
-					and o.obs_datetime between :startDate and :endDate and e.location_id=:location 
-				) list_cd4
-					inner join person per on per.person_id=list_cd4.patient_id 
-		    			inner join obs o on o.person_id=list_cd4.patient_id and list_cd4.data_cd4=o.obs_datetime and o.voided=0  
-		    			and per.voided=0 and timestampdiff(month,per.birthdate,:endDate)<12 and o.concept_id = 1695 and o.value_numeric<750 and o.location_id=:location 
+       	select distinct max_cd4.patient_id,max_data_cd4,o.value_numeric 
+       	from ( 
+    				select p.patient_id,max(o.obs_datetime) max_data_cd4  
+    				from patient p 
+    					inner join encounter e on p.patient_id=e.patient_id 
+    					inner join obs o on e.encounter_id=o.encounter_id 
+    				where p.voided=0 and e.voided=0 and o.voided=0 and concept_id = 1695 and  e.encounter_type in (6,51,13,53,90) 
+    					and o.obs_datetime between :startDate and :endDate and e.location_id=:location 
+    					group by p.patient_id 
+    		)max_cd4 
+    			inner join person per on per.person_id=max_cd4.patient_id 
+    			inner join obs o on o.person_id=max_cd4.patient_id and max_cd4.max_data_cd4=o.obs_datetime and o.voided=0  
+   			and per.voided=0 and timestampdiff(month,per.birthdate,:endDate)>=12 and timestampdiff(year,per.birthdate,:endDate)<5 and o.concept_id = 1695 and o.value_numeric<500 and o.location_id=:location 
             
-			)
-			max_cd4 group by patient_id
-        	) max_cd4
+       	union 
+       	
+       	select distinct max_cd4.patient_id,max_data_cd4,o.value_numeric 
+       	from( 
+    			select p.patient_id,max(o.obs_datetime) max_data_cd4 
+    			from patient p 
+    				inner join encounter e on p.patient_id=e.patient_id 
+    				inner join obs o on e.encounter_id=o.encounter_id 
+    			where p.voided=0 and e.voided=0 and o.voided=0 and concept_id = 1695 and  e.encounter_type in (6,51,13,53,90) 
+    				and o.obs_datetime between :startDate and :endDate and e.location_id=:location 
+    				group by p.patient_id 
+    		)max_cd4 
+    			inner join person per on per.person_id=max_cd4.patient_id 
+    			inner join obs o on o.person_id=max_cd4.patient_id and max_cd4.max_data_cd4=o.obs_datetime and o.voided=0  
+    			and per.voided=0 and timestampdiff(month,per.birthdate,:endDate)<12 and o.concept_id = 1695 and o.value_numeric<750 and o.location_id=:location 
     	) cd4 group by patient_id 
 ) cd4EligibilidadeMDSDAH on inicioDAH.patient_id = cd4EligibilidadeMDSDAH.patient_id  
 left join( 
