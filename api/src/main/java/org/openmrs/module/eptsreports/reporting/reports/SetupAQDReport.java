53ea668eab9a10a58d1b6cc8fae7957f94044815
emaposse <eusebiomaposse@gmail.com>
2024-08-27 18:55:42 +0200

    SESP-299:Remove Summary.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupAQDReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupAQDReport.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
-
 import org.openmrs.module.eptsreports.reporting.library.cohorts.GenericCohortQueries;
 import org.openmrs.module.eptsreports.reporting.library.datasets.DatimCodeDataSet;
 import org.openmrs.module.eptsreports.reporting.library.datasets.LocationDataSetDefinition;
